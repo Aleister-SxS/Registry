ac16e56bf80574473279d397fd889b6253d80c42
Aleister <aleistercazar@gmail.com>
2026-06-19 03:49:13 -0700

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
 // Place this file in the ROOT of your GitHub Pages repo (same level as index.html)
-// Version: 2026061901
+// Version: 2026061902
 
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
